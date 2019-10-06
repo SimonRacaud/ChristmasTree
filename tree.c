5141c7b09e52df0c1d488f2788dd3b2bf8817399
simon <simon@localhost.localdomain>
2019-10-06 15:20:53 +0200

    Passage parametre fonction > const

--- tree.c
+++ tree.c
@@ -6,36 +6,36 @@
 */
 
 int my_putchar(char c);
-void calc_height_width(int *size, int *tree_height, int *tree_width);
-void display_foliage(int *tree_height, int *tree_width);
-void display_foliage_char(int *tree_width, int *curr_width);
-void display_trunk(int *size, int *tree_width);
+void calc_height_width(const int size, int *tree_height, int *tree_width);
+void display_foliage(const int *tree_height, const int *tree_width);
+void display_foliage_char(const int *tree_width, const int *curr_width);
+void display_trunk(const int *size, const int *tree_width);
 
 void tree(int size)
 {
     int tree_height = 0;
     int tree_width = -1;
 
-    calc_height_width(&size, &tree_height, &tree_width);
+    calc_height_width(size, &tree_height, &tree_width);
     display_foliage(&tree_height, &tree_width);
     display_trunk(&size, &tree_width);
 }
 
-void calc_height_width(int *size, int *tree_height, int *tree_width)
+void calc_height_width(const int size, int *tree_height, int *tree_width)
 {
     int bloc_size = 4;
     int bloc_space = 2;
 
-    for (int i = 0; i < *size; i++) {
+    for (int i = 0; i < size; i++) {
         *tree_height += bloc_size;
         bloc_size++;
     }
     bloc_size = 4;
-    for (int i = 0; i < *size; i++) {
+    for (int i = 0; i < size; i++) {
         for (int j = 0; j < bloc_size; j++) {
             *tree_width += 2;
         }
-        if (i != *size-1)
+        if (i != size - 1)
             *tree_width -= (bloc_space + 2);
         if ((i + 1) % 2 == 0)
             bloc_space += 2;
@@ -43,7 +43,7 @@ void calc_height_width(int *size, int *tree_height, int *tree_width)
     }
 }
 
-void display_foliage(int *tree_height, int *tree_width)
+void display_foliage(const int *tree_height, const int *tree_width)
 {
     int bloc_size = 4;
     int bloc_space = 2;
@@ -66,7 +66,7 @@ void display_foliage(int *tree_height, int *tree_width)
     }
 }
 
-void display_foliage_char(int *tree_width, int *curr_width)
+void display_foliage_char(const int *tree_width, const int *curr_width)
 {
     for (int k=0; k < ((*tree_width / 2) - (*curr_width / 2)); k++)
         my_putchar(' ');
@@ -75,14 +75,14 @@ void display_foliage_char(int *tree_width, int *curr_width)
     my_putchar('\n');
 }
 
-void display_trunk(int *size, int *tree_width)
+void display_trunk(const int *size, const int *tree_width)
 {
     int width_trunk = *size;
     if (*size > 1 && ((*size - 1) % 2) != 0) {
-        width_trunk = *size+1;
+        width_trunk = *size + 1;
     }
     for (int i = 0; i < *size; i++) {
-        for (int k = 0; k < ((*tree_width/2)-(width_trunk/2)); k++) {
+        for (int k = 0; k < ((*tree_width / 2) - (width_trunk / 2)); k++) {
             my_putchar(' ');
         }
         for (int j = 0; j < width_trunk; j++) {
